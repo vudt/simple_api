d7f122c6fa124691eb43cdee3752364f5cc248a3
Vu Dang <dangtuanvu1990@gmail.com>
2019-08-02 17:29:28 +0700

    delete folder

--- spec/factories/users.rb
+++
@@ -1,9 +0,0 @@
-require 'faker'
-FactoryBot.define do
-  factory :user do
-    email { Faker::Internet.email }
-    password {"123456"}
-    password_confirmation {"123456"}
-    authentication_token { Faker::Code.imei }
-  end
-end
